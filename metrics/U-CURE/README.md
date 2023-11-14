c32d29f490efcbb3f019f62fdf4cd6418b10048a
gadevito <125179321+gadevito@users.noreply.github.com>
2023-11-14 11:35:08 +0100

    Add files via upload

--- metrics/U-CURE/README.md
+++ metrics/U-CURE/README.md
@@ -1,1 +1,7 @@
-
+# U-CURE Metrics
+The `U-CURE` directory contains the source code used to evaluate the 1-Rouge and BLEU metrics for the U-CURE use case model.
+It contains the following files:
+- U-CURE_DG.py, evaluates the Data Groups metrics.
+- U-CURE_DM.py, evaluates the Data Movements metrics.
+- U-CURE_FU.py, evaluates the Funtional Users metrics.
+- U-CURE_OOI.py, evaluates the Objects of Interest metrics.
---
+++ metrics/U-CURE/U-CURE_OOI.py
@@ -0,0 +1,139 @@
+from rouge import Rouge
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu, sentence_bleu
+
+
+
+
+
+model_out = [ 
+#1
+["None","None","Dataset","None","Dataset","ML Engine","EDA","EDA","Variable","None","ML Engine","Transformation","Transformation","None","Message"
+],
+#2
+["Project","Project","Project","None","None","Message"
+],
+#3
+["Project","Project","Project","None","None","Message"
+],
+#4
+["Variable","ML Engine","Analysis","Analysis","Variable","None","ML Engine","T-test","T-test",
+],
+#5
+["Feature","ML Engine","Feature","Features"
+],
+#6
+["Feature","Feature","Analysis","Analysis"
+],
+#7
+["None","None","Feature","None","ML Engine","Analysis","Analysis"
+],
+#8
+["None","None","Model","None","Model","Model","Model","Model"
+],
+#9
+["None","None","Model","None","Model","Model","Model"
+],
+#10
+[
+"Patient","SVC model","Patient","None","Patient","Patient"
+]
+
+
+]
+
+reference = [
+#1
+["None","None","Project","None","Project","Project","EDA","EDA","Variable","None","Variable","Variable","Variable","None","Message"
+],
+#2
+["Project","Project","Project","None","None","Message"
+],
+#3
+["Project","Project","Project","None","None","Message"
+],
+#4
+["Variable","Variable","Analysis","Analysis","Variable","None","Variable","T-test","T-test"
+],
+#5
+["Feature","Feature","Feature","Feature"
+],
+#6
+["Feature","Feature","Analysis","Analysis"
+],
+#7
+["None","None","Feature","None","Feature","Analysis","Analysis"
+],
+#8
+["None","None","Model","None","Model","Model","Model","Model"
+],
+#9
+["None","None","Model","None","Model","Model","Model"
+],
+#10
+[
+    "Patient","Model","Patient","Model","Patient","Patient"
+]
+]
+rouge = Rouge()
+
+
+totalDG = 0
+avgB = 0
+avgR = [0, 0, 0]
+avgRL = [0, 0, 0]
+
+for i in range(0,len(reference)):
+    r = []
+    m = []
+    totalDG = totalDG + len(reference[i])
+    for j in reference[i]:
+        ss = j.split()
+
+        for k in ss:
+            r.append(k)
+
+    for j in model_out[i]:
+        ss = j.split()
+        for k in ss:
+            m.append(k)
+    
+    cc = SmoothingFunction()
+    c = sentence_bleu([r],m, weights=(1, 0, 0, 0),smoothing_function=cc.method4)
+    avgB = avgB + c
+
+    cx = rouge.get_scores(model_out[i], reference[i], avg=True)
+    
+    r1 = cx['rouge-1']
+    r2 = cx['rouge-l']
+    avgR[0] = avgR[0] + r1['p']
+    avgR[1] = avgR[1] + r1['r']
+    avgR[2] = avgR[2] + r1['f']
+    
+    avgRL[0] = avgRL[0] + r2['p']
+    avgRL[1] = avgRL[1] + r2['r']
+    avgRL[2] = avgRL[2] + r2['f']
+
+    r1['p'] = "{0:.2%}".format(r1['p'])
+    r1['r'] = "{0:.2%}".format(r1['r'])
+    r1['f'] = "{0:.2%}".format(r1['f'])
+    r2['p'] = "{0:.2%}".format(r2['p'])
+    r2['r'] = "{0:.2%}".format(r2['r'])
+    r2['f'] = "{0:.2%}".format(r2['f'])
+    c = "{0:.2%}".format(c)
+    print(i+1,') ', "Rouge-1 ", r1, "Rouge-l", r2, "Bleu ", c)
+
+avgB = avgB/len(reference)
+#avg = sentence_bleu([r],m)
+print("AVG BLEU: ", "{0:.2%}".format(avgB))
+
+avgR[0] = avgR[0]/len(reference)
+avgR[1] = avgR[1]/len(reference)
+avgR[2] = avgR[2]/len(reference)
+
+avgRL[0] = avgRL[0]/len(reference)
+avgRL[1] = avgRL[1]/len(reference)
+avgRL[2] = avgRL[2]/len(reference)
+
+print("AVG ROUGE-1: ", "Precision ","{0:.2%}".format(avgR[0]), "Recall ", "{0:.2%}".format(avgR[1]),"F1-score ", "{0:.2%}".format(avgR[2]))
+print("AVG ROUGE-L: ", "Precision ","{0:.2%}".format(avgRL[0]), "Recall ", "{0:.2%}".format(avgRL[1]),"F1-score ", "{0:.2%}".format(avgRL[2]))
+print("Total OOI ", totalDG)
