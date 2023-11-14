27848f37e55490150a26f5234b4c70541db778de
gadevito <125179321+gadevito@users.noreply.github.com>
2023-11-14 11:32:20 +0100

    Add files via upload

---
+++ metrics/Realtime/Automatic Line Switching/ALS_FU.py
@@ -0,0 +1,183 @@
+from rouge import Rouge
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu, sentence_bleu
+
+model_out = [ 
+#1
+["CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+
+],
+#2
+["CC Panel",
+"Empty",
+"Empty","Switch device",
+"CC Panel"
+
+],
+#3
+["CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+
+],
+#4
+[
+"CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+],
+#5
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC panel"
+],
+#6
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+],
+#7
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"    
+]
+]
+
+reference = [
+#1
+["CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+
+],
+#2
+["CC Panel",
+"Empty",
+"Empty","Switch device",
+"CC Panel"
+
+],
+#3
+["CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+
+],
+#4
+[
+"CC Panel",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+],
+#5
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC panel"
+],
+#6
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"
+],
+#7
+[
+"QLCM",
+"Empty",
+"Empty",
+"Switch device",
+"CC Panel"    
+]
+]
+
+
+rouge = Rouge()
+
+
+totalDG = 0
+avgB = 0
+avgR = [0, 0, 0]
+avgRL = [0, 0, 0]
+
+for i in range(0,len(reference)):
+    r = []
+    m = []
+    totalDG = totalDG + len(reference[i])
+    for j in reference[i]:
+        ss = j.split()
+
+        for k in ss:
+            r.append(k)
+
+    for j in model_out[i]:
+        ss = j.split()
+        for k in ss:
+            m.append(k)
+    
+    cc = SmoothingFunction()
+    c = sentence_bleu([r],m, weights=(1, 0, 0, 0),smoothing_function=cc.method4)
+    avgB = avgB + c
+
+    cx = rouge.get_scores(model_out[i], reference[i], avg=True)
+    
+    r1 = cx['rouge-1']
+    r2 = cx['rouge-l']
+    avgR[0] = avgR[0] + r1['p']
+    avgR[1] = avgR[1] + r1['r']
+    avgR[2] = avgR[2] + r1['f']
+    
+    avgRL[0] = avgRL[0] + r2['p']
+    avgRL[1] = avgRL[1] + r2['r']
+    avgRL[2] = avgRL[2] + r2['f']
+
+    r1['p'] = "{0:.2%}".format(r1['p'])
+    r1['r'] = "{0:.2%}".format(r1['r'])
+    r1['f'] = "{0:.2%}".format(r1['f'])
+    r2['p'] = "{0:.2%}".format(r2['p'])
+    r2['r'] = "{0:.2%}".format(r2['r'])
+    r2['f'] = "{0:.2%}".format(r2['f'])
+    c = "{0:.2%}".format(c)
+    print(i+1,') ', "Rouge-1 ", r1, "Rouge-l", r2, "Bleu ", c)
+
+avgB = avgB/len(reference)
+#avg = sentence_bleu([r],m)
+print("AVG BLEU: ", "{0:.2%}".format(avgB))
+
+avgR[0] = avgR[0]/len(reference)
+avgR[1] = avgR[1]/len(reference)
+avgR[2] = avgR[2]/len(reference)
+
+avgRL[0] = avgRL[0]/len(reference)
+avgRL[1] = avgRL[1]/len(reference)
+avgRL[2] = avgRL[2]/len(reference)
+
+print("AVG ROUGE-1: ", "Precision ","{0:.2%}".format(avgR[0]), "Recall ", "{0:.2%}".format(avgR[1]),"F1-score ", "{0:.2%}".format(avgR[2]))
+print("AVG ROUGE-L: ", "Precision ","{0:.2%}".format(avgRL[0]), "Recall ", "{0:.2%}".format(avgRL[1]),"F1-score ", "{0:.2%}".format(avgRL[2]))
+print("Total FU ", totalDG)
--- metrics/Realtime/Automatic Line Switching/README.md
+++ metrics/Realtime/Automatic Line Switching/README.md
@@ -1,1 +1,7 @@
-
+# Automatic Line Switching Metrics
+The `Automatic Line Switching` directory contains the source code used to evaluate the 1-Rouge and BLEU metrics for the Automatic Line Switching use case model.
+It contains the following files:
+- ALS_DG.py, evaluates the Data Groups metrics.
+- ALS_DM.py, evaluates the Data Movements metrics.
+- ALS_FU.py, evaluates the Funtional Users metrics.
+- ALS_OOI.py, evaluates the Objects of Interest metrics.
